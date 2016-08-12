8dbfa261be74ece3498afc20665fff564221e8ec
niney <kpeteras@gmail.com>
2016-08-12 09:20:07 +0900

    - grunt server 구성

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,87 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     // Automatically load required Grunt tasks
-    require('jit-grunt')(grunt, {
-    });
+    require('jit-grunt')(grunt, {});
+
+    // Configurable paths for the application
+    var appConfig = {
+        app: require('./bower.json').appPath || 'app',
+        dist: 'dist'
+    };
 
     // Define the configuration for all the tasks
     grunt.initConfig({
 
+        // Project settings
+        yeoman: appConfig,
+
         // Automatically inject Bower components into the app
         wiredep: {
             app: {
-                src: ['module.html'],
+                src: ['<%= yeoman.app %>/index.html'],
                 ignorePath: /\.\.\//
             }
+        },
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            bower: {
+                files: ['bower.json'],
+                tasks: ['wiredep']
+            },
+            js: {
+                files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= yeoman.app %>/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect) {
+                        return [
+                            connect().use(
+                                '/bower_components',
+                                connect.static('./bower_components')
+                            ),
+                            connect.static(appConfig.app)
+                        ];
+                    }
+                }
+            }
         }
+
+    });
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+        grunt.task.run([
+            'wiredep',
+            'connect:livereload',
+            'watch'
+        ]);
     });
 
     grunt.registerTask('default', [
